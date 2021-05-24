1b4af507791232d76abca835922b2b679c7f9cc8
Schaffer <mr.giancarloschaffer@gmail.com>
2021-05-24 16:23:53 -0300

    Added logo on home screen

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,10 +2,12 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "../Style/style.scss";
 import { touchVibe } from "../Misc/utils";
+import logo from "../logo512.png";
 
 const Home = () => {
   return (
     <div className="body-home">
+      <img src={logo} alt="password logo" height={110} />
       <Link to="/game" onClick={() => touchVibe()}>
         <button type="button" className="nes-btn">
           START
